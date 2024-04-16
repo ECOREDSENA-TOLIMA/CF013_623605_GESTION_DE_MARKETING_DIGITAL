ac49f586340da827a9af732f044c167388b8167c
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2024-04-16 14:45:31 -0500

    Ajustes 4

--- src/config/global.js
+++ src/config/global.js
@@ -134,7 +134,7 @@ export default {
       {
         icono: 'fas fa-download',
         titulo: 'Descargar PDF',
-        download: 'downloads/CFA_013_623605_DU.pdf',
+        download: 'downloads/CFA_13_623605_DU.pdf',
       },
       {
         icono: 'fas fa-download',
